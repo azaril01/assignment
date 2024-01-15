4c50d724d87c50bcd59f05b0c9070b61df632696
azaril01 <128761001+azaril01@users.noreply.github.com>
2024-01-15 18:28:04 +0800

    updateagain

--- index.js
+++ index.js
@@ -1385,7 +1385,7 @@ app.get('/security/visitor-pass/:identification_No/host-contact', async function
 
 
 app.get('/', (req, res)=>{
-    res.send("Testing deployment from zaidzaihan.azurewebsites.net");
+    res.send("Testing deployment from loner.azurewebsites.net");
 });
 
 
